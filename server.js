624ae85c934739c691ebf8a061b37090fdc7313b
Prashmit Tanay <tanay.wsd@nic.in>
2014-03-05 17:39:38 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var mime = require('mime');
+var cache = {};
+
+function send404(response){
+	response.writeHead(404, {'Content-Type':'text/plain'});
+	response.write('Error 404: Resource Not Found');
+	response.end();
+}
+
+function sendFile(response, filePath, fileContents){
+	response.writeHead(
+		200,
+		{"content-type":mime.lookup(path.basename(filePath))}
+	);
+	response.end(fileContents);
+}
