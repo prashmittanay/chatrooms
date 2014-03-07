9c4f183cf37f285094a9c211fae2a52bd518f6b9
Prashmit Tanay <tanay.wsd@nic.in>
2014-03-07 17:07:22 +0530

    chat server helper file added

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path');
 var mime = require('mime');
 var cache = {};
 
+
 function send404(response){
 	response.writeHead(404, {'Content-Type':'text/plain'});
 	response.write('Error 404: Resource Not Found');
@@ -52,6 +53,10 @@ var server = http.createServer(function(request, response){
 	serveStatic(response, cache, absPath);
 });
 
+
 server.listen(3000, function(){
 	console.log('Server listening on port 3000');
 });
+
+var chatServer = require('./lib/chat_server');
+chatServer.listen(server);
